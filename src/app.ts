9ae8378397a7fabe664d214b03ee4b5b13905536
developer-shourav <mrinmoyraj823@gmail.com>
2024-12-12 19:37:53 +0600

    Route Refactoring completed

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/students/student.routes';
-import { UserRoutes } from './app/modules/user/user.routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -11,13 +10,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-/* ------- Application Routes---------- */
-
-/* ------- User Route */
-app.use('/api/v1/users', UserRoutes);
-
-/* ------- Students Route */
-app.use('/api/v1/students', StudentRoutes);
+/* ------- Root Route ----------*/
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to University Manager! 🎇✨🎉');
