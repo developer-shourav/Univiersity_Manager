996cf714a9332523304d04b1164fc24f6d07bbf6
developer-shourav <mrinmoyraj823@gmail.com>
2024-12-14 18:35:38 +0600

    Data Validator Middleware added

--- src/app/modules/students/student.validation.ts
+++ src/app/modules/students/student.validation.ts
@@ -24,20 +24,26 @@ const localGuardianValidationSchema = z.object({
 });
 
 // Define the main Student schema
-export const studentValidationSchema = z.object({
-  id: z.string().trim(),
-  name: userNameValidationSchema,
-  gender: z.enum(['male', 'female', 'other']),
-  dateOfBirth: z.string().trim(), // Assuming date is in string format
-  email: z.string().email().trim(),
-  contactNumber: z.string().trim(),
-  emergencyContactNo: z.string().trim(),
-  bloodGroup: z.enum(['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-']),
-  presentAddress: z.string().trim(),
-  permanentAddress: z.string().trim(),
-  guardian: guardianValidationSchema,
-  localGuardian: localGuardianValidationSchema,
-  profileImage: z.string().optional(), // Assuming URLs for profile images
-  isActive: z.enum(['active', 'blocked']).default('active'),
-  isDeleted: z.boolean(),
+const createStudentValidationSchema = z.object({
+  body: z.object({
+    password: z.string().max(20),
+    student: z.object({
+      name: userNameValidationSchema,
+      gender: z.enum(['male', 'female', 'other']),
+      dateOfBirth: z.string().trim(), // Assuming date is in string format
+      email: z.string().email().trim(),
+      contactNumber: z.string().trim(),
+      emergencyContactNo: z.string().trim(),
+      bloodGroup: z.enum(['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-']),
+      presentAddress: z.string().trim(),
+      permanentAddress: z.string().trim(),
+      guardian: guardianValidationSchema,
+      localGuardian: localGuardianValidationSchema,
+      profileImage: z.string().optional(), // Assuming URLs for profile images
+    }),
+  }),
 });
+
+export const studentValidations = {
+ createStudentValidationSchema,
+};
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { studentValidations } from '../students/student.validation';
 import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
 // -----------Create A Student
-router.post('/create-student', UserControllers.createStudent);
+router.post(
+  '/create-student',
+  validateRequest(studentValidations.createStudentValidationSchema),
+  UserControllers.createStudent,
+);
 
 export const UserRoutes = router;
