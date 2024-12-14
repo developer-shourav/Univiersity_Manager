ac21ec431e998092d5e4c2d1c3d089d0ca57a62f
developer-shourav <mrinmoyraj823@gmail.com>
2024-12-14 14:30:31 +0600

    Controller code refector completed

--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -1,64 +1,50 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, RequestHandler, Response, NextFunction } from 'express';
 import { studentServices } from './student.service';
 import sendResponse from '../../utils/sendResponse';
 
-/* ----------------------Get All Student----------------- */
-const getAllStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    // 1. Will call service function to get all Students
-    const result = await studentServices.getAllStudentsFromDB();
-
-    // 2. Send Response to the frontend
-    sendResponse(res, {
-      message: 'All students are retrieved successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
 };
+
+/* ----------------------Get All Student----------------- */
+const getAllStudent = catchAsync(async (req, res, next) => {
+  // 1. Will call service function to get all Students
+  const result = await studentServices.getAllStudentsFromDB();
+
+  // 2. Send Response to the frontend
+  sendResponse(res, {
+    message: 'All students are retrieved successfully',
+    data: result,
+  });
+});
 
 /* ----------------------Get Single Student----------------- */
-const getAStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    // 1. Will Call service function to get the student using id
-    const { studentId } = req.params;
-    const result = await studentServices.getAStudentFromDB(studentId);
+const getAStudent = catchAsync(async (req, res, next) => {
+  // 1. Will Call service function to get the student using id
+  const { studentId } = req.params;
+  const result = await studentServices.getAStudentFromDB(studentId);
 
-    // 2. Send Response to the frontend
-    sendResponse(res, {
-      message: 'Student retrieved successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  // 2. Send Response to the frontend
+  sendResponse(res, {
+    message: 'Student retrieved successfully',
+    data: result,
+  });
+});
 
 /* ----------------------Delete Single Student----------------- */
-const deleteAStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    // 1. Will Call service function to get the student using id
-    const { studentId } = req.params;
-    const result = await studentServices.deleteAStudentFromDB(studentId);
+const deleteAStudent = catchAsync(async (req, res, next) => {
+  // 1. Will Call service function to get the student using id
+  const { studentId } = req.params;
+  const result = await studentServices.deleteAStudentFromDB(studentId);
 
-    // 2. Send Response to the frontend
-    sendResponse(res, {
-      message: 'Student Delete successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  // 2. Send Response to the frontend
+  sendResponse(res, {
+    message: 'Student Delete successfully',
+    data: result,
+  });
+});
 
 export const StudentControllers = {
   getAllStudent,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,30 +1,28 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { UserServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 
-/* ----------------------Create A Student----------------- */
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student: createNewStudentData } = req.body;
-
-    // will call service function to send this data
-    const result = await UserServices.createStudentIntoDB(
-      password,
-      createNewStudentData,
-    );
-
-    sendResponse(res, {
-      message: 'Student is created successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
 };
+
+/* ----------------------Create A Student----------------- */
+const createStudent = catchAsync(async (req, res, next) => {
+  const { password, student: createNewStudentData } = req.body;
+
+  // will call service function to send this data
+  const result = await UserServices.createStudentIntoDB(
+    password,
+    createNewStudentData,
+  );
+
+  sendResponse(res, {
+    message: 'Student is created successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
