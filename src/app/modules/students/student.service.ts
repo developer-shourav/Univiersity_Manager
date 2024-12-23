8320bb3f8a542fe8dc05f73ce5d390314307ff4c
developer-shourav <mrinmoyraj823@gmail.com>
2024-12-23 23:53:05 +0600

    Populate and Nested Populate added

--- src/app/modules/students/student.service.ts
+++ src/app/modules/students/student.service.ts
@@ -2,17 +2,32 @@ import { Student } from './student.model';
 
 /* --------Logic For Get All Students------ */
 const getAllStudentsFromDB = async () => {
-  const result = await Student.find();
+  const result = await Student.find()
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
   return result;
 };
 
 /* --------Logic For Get A Students------ */
 const getAStudentFromDB = async (id: string) => {
   // using query
-  // const result = await Student.findOne({ id });
+  const result = await Student.findOne({ id })
+    .populate('user')
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
 
   // using aggregation
-  const result = await Student.aggregate([{ $match: { id: id } }]);
+  // const result = await Student.aggregate([{ $match: { id: id } }]);
 
   return result;
 };
