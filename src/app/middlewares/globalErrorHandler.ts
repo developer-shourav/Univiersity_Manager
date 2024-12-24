60658dac73f7585d318acb4148e0ccef3a48f998
developer-shourav <mrinmoyraj823@gmail.com>
2024-12-25 01:01:18 +0600

    Error type handler added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,16 +1,52 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  const statusCode = err.status || 500; // Use a status code from the error if available
-  const errorMsg = err.message || 'Something went wrong!';
+  // Setting Default Values
+  let statusCode = err.status || 500; // Use a status code from the error if available
+  let message = err.message || 'Something went wrong!';
 
-  res.status(statusCode).json({
+  type TErrorSource = {
+    path: string | number | undefined | null;
+    message: string;
+  }[];
+
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  /* ---------Ensure the error type ----------- */
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'Ami Zod Error';
+  }
+
+  return res.status(statusCode).json({
     success: false,
-    message: errorMsg,
+    message,
+    errorSources,
     error: err,
   });
 };
 
 export default globalErrorHandler;
+
+//------------- Our targeted error pattern -------------
+/*
+
+success: false,
+message: 'Something went wrong!',
+errorSources:[
+  path:'',
+  message:''
+]
+stack: 'Error stack trace'
+
+
+
+*/
