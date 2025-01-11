5e5faf8c37c3e8974d6aca5662e965768fc3db59
developer-shourav <mrinmoyraj823@gmail.com>
2025-01-11 14:37:50 +0600

    Auth Module Stracture added

---
+++ src/app/modules/Auth/auth.routes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AuthValidation } from './auth.validation';
+import { AuthControllers } from './auth.controller';
+
+
+const router = express.Router();
+
+/* -------Post Request Log In an User */
+router.post(
+    '/login',
+    validateRequest(AuthValidation.loginValidationSchema),
+    AuthControllers.loginUser
+  );
+  
+
+
+export const AuthRoutes = router;
---
+++ src/app/modules/Auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { TLoginUser } from "./auth.interface";
+
+const logInUser = async(payload: TLoginUser) => {
+    return {};
+};
+
+
+export const AuthServices = {
+   logInUser,
+};
