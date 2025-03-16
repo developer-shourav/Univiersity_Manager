43ca5bc7362370307f37e0ef8be290b575b1f02f
developer-shourav <mrinmoyraj823@gmail.com>
2025-03-16 22:11:35 +0600

    Enrolled Student In a course successfully

--- src/app/modules/EnrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -5,6 +5,7 @@ import { OfferedCourse } from '../OfferedCourse/OfferedCourse.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
 import EnrolledCourse from './enrolledCourse.model';
 import { Student } from '../students/student.model';
+import mongoose from 'mongoose';
 
 
 const createEnrolledCourseIntoDB = async (
@@ -20,40 +21,77 @@ const createEnrolledCourseIntoDB = async (
 
    */
 
-/* ------------heck if the offered courses is exists----------- */
-const {offeredCourse} = payload;
-const isOfferedCourseExist = await OfferedCourse.findById(offeredCourse);
-if(!isOfferedCourseExist){
-  throw new AppError(httpStatus.NOT_FOUND, 'Offered course not found !');
-}
+  /* ------------heck if the offered courses is exists----------- */
+  const { offeredCourse } = payload;
+  const isOfferedCourseExist = await OfferedCourse.findById(offeredCourse);
+  if (!isOfferedCourseExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Offered course not found !');
+  }
 
-/* ------------ Check if the max capacity of the offered course exceed ----------- */
-if(isOfferedCourseExist.maxCapacity <= 0){
-  throw new AppError(httpStatus.BAD_REQUEST, 'Room is full !');
-}
+  /* ------------ Check if the max capacity of the offered course exceed ----------- */
+  if (isOfferedCourseExist.maxCapacity <= 0) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Room is full !');
+  }
 
-/* ------------Find the Student to get student objectId---------- */
-const student = await Student.findOne({id: userId}).select('id');
-if(!student){
-  throw new AppError(httpStatus.NOT_FOUND, 'Student is not found !');
-}
+  /* ------------Find the Student to get student objectId---------- */
+  const student = await Student.findOne({ id: userId }).select('id');
+  if (!student) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Student is not found !');
+  }
 
-/* ------------Check if the student is already enrolled----------- */
-const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({semesterRegistration: isOfferedCourseExist?.semesterRegistration, offeredCourse, student: student._id});
+  /* ------------Check if the student is already enrolled----------- */
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({ semesterRegistration: isOfferedCourseExist?.semesterRegistration, offeredCourse, student: student._id });
 
-if(isStudentAlreadyEnrolled){
-  throw new AppError(httpStatus.CONFLICT, 'Student already enrolled');
-}
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(httpStatus.CONFLICT, 'Student already enrolled');
+  }
 
 
+  /* ----------Start Session for write-------- */
+  const session = await mongoose.startSession();
 
-  return null
+  try {
+    session.startTransaction();
+    const result = await EnrolledCourse.create([{
+      semesterRegistration: isOfferedCourseExist.semesterRegistration,
+      academicSemester: isOfferedCourseExist.academicSemester,
+      academicFaculty: isOfferedCourseExist.academicFaculty,
+      academicDepartment: isOfferedCourseExist.academicDepartment,
+      offeredCourse: offeredCourse,
+      course: isOfferedCourseExist.course,
+      student: student._id,
+      faculty: isOfferedCourseExist.faculty,
+      isEnrolled: true,
+    }], {session});
+
+    if (!result) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to enrolled in this course !');
+    }
+
+    const maxCapacity = isOfferedCourseExist.maxCapacity;
+
+    await OfferedCourse.findByIdAndUpdate(offeredCourse, {
+      maxCapacity: maxCapacity - 1,
+    });
+
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return result;
+
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(`${err}`);
+  }
+
 };
 const updateEnrolledCourseMarksIntoDB = async (
   facultyId: string,
   payload: Partial<TEnrolledCourse>,
 ) => {
-  return null;
+  return {facultyId, payload};
 };
 
 export const EnrolledCourseServices = {
